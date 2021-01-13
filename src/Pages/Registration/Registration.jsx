9b39479871ecf0eb5c3e18d93e65d6d5113a5901
bdzanko17 <bdzanko1@gmail.com>
2021-01-13 14:18:38 +0100

    new registration

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,34 +1,72 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+async function loginUser(credentials) {
+    console.log(JSON.stringify(credentials))
+    return fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
 
 const Registration = (props) => {
 
-    return(
-        <div className="Registration_main">
-        <div className="Textbox_main">
-            <div className="textbox_submain">
-                <label for="name">Name</label>
-                <input type="text" id="name"/>
-                <label for="email">E-mail</label>
-                <input type="text" id="email"/>
-                <label for="password">Password</label>
-                <input type="text" id="password"/>
-            </div>
-            <button className="textbox_button">
-                Registration
-            </button>
-            <Link to = "/">
-                    <button className="Back">Back</button>
-            </Link>
-        </div>
-        <div className="Text_main">
-            <h2 className="sdp">SOFTVERSKI DIZAJN PROTOKOLA</h2>
-            <p className="etf">Faculty of Electrical Engeneering</p>
-            <p className="dot">Department of Telecommunications</p>
-        </div>
+    const [name, setUserName] = useState();
+    const [password, setPassword] = useState();
+    const [email, setEmail] = useState()
+    const [role, setRole] = useState()
 
-    </div>
-    );
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const response = await loginUser({
+            name,
+            password,
+            email,
+            role
+        });
+        console.log(response)
+    }
+
+    return (
+        <div className="login-wrapper">
+            <h1>Please Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p>Name</p>
+                    <input type="text" onChange={e => setUserName(e.target.value)} />
+                </label>
+                <label>
+                    <p>Email</p>
+                    <input type="email" onChange={e => setPassword(e.target.value)} />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" onChange={e => setEmail(e.target.value)} />
+                </label>
+                <label>
+                    <p>Role</p>
+                    <input type="text" onChange={e => setRole(e.target.value)} />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+            <Link to="/">
+                <button className="Back">Back</button>
+            </Link>
+            <div className="Text_main">
+                <h2 className="sdp">SOFTVERSKI DIZAJN PROTOKOLA</h2>
+                <p className="etf">Faculty of Electrical Engeneering</p>
+                <p className="dot">Department of Telecommunications</p>
+            </div>
+        </div>
+    )
+
 }
 
 export default Registration;
